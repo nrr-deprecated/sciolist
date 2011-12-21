ce4c8489509be49bf4c0b165742722e64e47f2a8
Nathaniel R Reindl <nrr@corvidae.org>
2011-12-21 14:18:19 +0000

    Add more structure.

---
+++ lib/App/sciolist/Application/Command.pm
@@ -0,0 +1,16 @@
+package App::sciolist::Application::Command;
+use App::Cmd::Setup -command;
+
+sub opt_spec {
+    my ( $class, $app ) = @_;
+    return ([ 'help' => "This usage screen" ],
+	    $class->options($app));
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+    die $self->_usage_text if $opt->{help};
+    $self->validate( $opt, $args );
+}
+
+1;
