397ea05ecc227aaa55b7b1238d389a64e40bdfbc
Nathaniel R Reindl <nrr@corvidae.org>
2011-12-21 17:43:09 +0000

    Add in more structure.

---
+++ lib/App/sciolist/Application/Command/Key.pm
@@ -0,0 +1,23 @@
+package App::sciolist::Application::Command::Key;
+use base qw[ App::sciolist::Application::Command ];
+use common::sense;
+
+sub notify_of_subcommand_dispatch {
+}
+
+sub option_spec {
+}
+
+sub run {
+}
+
+sub subcommand_alias {
+}
+
+sub usage_text {
+}
+
+sub validate {
+}
+
+1;
