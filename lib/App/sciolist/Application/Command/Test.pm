2fdb8fd7db55d44fa86ce1271436744c637b210e
Nathaniel R Reindl <nrr@corvidae.org>
2012-03-19 12:09:35 -0500

    Why not?

---
+++ lib/App/sciolist/Application/Command/Test.pm
@@ -0,0 +1,23 @@
+package App::sciolist::Application::Command::Test;
+use base qw[ App::sciolist::Application::Command ];
+use strictures;
+
+sub notify_of_subcommand_dispatch {
+}
+
+sub option_spec {
+}
+
+sub run {
+}
+
+sub subcommand_alias {
+}
+
+sub usage_text {
+}
+
+sub validate {
+}
+
+1;
